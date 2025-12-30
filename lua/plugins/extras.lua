e1ef54e1b087adbf7cca1981f256372711bf3d14
IndieCoderMM <hthant00chk@gmail.com>
2025-12-30 17:54:50 +0000

    Add extras

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -3,7 +3,7 @@ return {
 	{
 		"vague-theme/vague.nvim",
 		lazy = false,
-		priority = 1000, -- make sure to load this before all the other plugins
+		priority = 1000,
 		config = function()
 			require("vague").setup({
 				transparent = false,
@@ -12,4 +12,87 @@ return {
 		end,
 	},
 	{ "wakatime/vim-wakatime", lazy = false },
+	{ -- Quick navs between buffers
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup({})
+
+			-- basic telescope configuration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			vim.keymap.set("n", ";e", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "Harpoon: [E]xplore window" })
+
+			vim.keymap.set("n", ";a", function()
+				harpoon:list():add()
+			end, { desc = "Harpoon: [A]dd current buffer" })
+
+			vim.keymap.set("n", ";r", function()
+				harpoon:list():remove()
+			end, { desc = "Harpoon: [R]emove current buffer" })
+
+			vim.keymap.set("n", ";p", function()
+				harpoon:list():prev()
+			end, { desc = "Harpoon: Toggle [P]revious buffer" })
+
+			vim.keymap.set("n", ";n", function()
+				harpoon:list():next()
+			end, { desc = "Harpoon: Toggle [N]ext buffer" })
+
+			vim.keymap.set("n", ";c", function()
+				harpoon:list():clear()
+			end, { desc = "Harpoon: [C]lear buffers list" })
+
+			-- vim.keymap.set('n', ';1', function()
+			--   harpoon:list():select(1)
+			-- end)
+			-- vim.keymap.set('n', ';2', function()
+			--   harpoon:list():select(2)
+			-- end)
+			-- vim.keymap.set('n', ';3', function()
+			--   harpoon:list():select(3)
+			-- end)
+			-- vim.keymap.set('n', ';4', function()
+			--   harpoon:list():select(4)
+			-- end)
+		end,
+	},
+	{ -- Highlight todo, notes, etc in comments
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+		keys = {
+			{ "<leader>st", "<cmd>TodoTelescope<CR>", desc = "[S]earch [T]odo" },
+		},
+	},
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = {
+			{ "<leader>tu", "<cmd>lua require('undotree').toggle()<cr>", desc = "[T]oggle [U]ndo Tree" },
+		},
+	},
 }
